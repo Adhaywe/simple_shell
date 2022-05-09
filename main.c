800f6cb25cde2a204c4cea801135792cbc6ce204
Adam <adamghebrezgi@gmail.com>
2022-05-09 18:02:17 +0300

    Program starts here and checks for commands to be executed

---
+++ main.c
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+#endif
