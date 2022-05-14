68eee3b9fd9456bac466e9e614209adf80305dcb
Adam <adamghebrezgi@gmail.com>
2022-05-14 09:55:44 +0300

    Header file

--- shell.h
+++ shell.h
@@ -8,5 +8,26 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <signal.h>
+
+#define EXIT
+
+extern char **environ;
+
+/* strtok functions */
+int token_length(char *string, char *delimiter);
+int number_of_tokens(char *string, char *delimiter);
+char **_strtok(char *line, char *delimiter);
+
+/* args control functions */
+int *get_args(char *line, int last_ret);
+int exec_args(char **commands, char **parameters, int *last_ret);
+int exec2_args(char **commands, char **parameters, int *last_ret);
+int control_args(int *last_ret);
+
+/* getline function */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 
 #endif
