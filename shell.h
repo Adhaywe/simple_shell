82a96302224b72ec139d29c71520beb25281f5ee
Adam <adamghebrezgi@gmail.com>
2022-05-14 11:40:28 +0300

    Header file

--- shell.h
+++ shell.h
@@ -14,13 +14,16 @@
 
 extern char **environ;
 
+/* signal handler */
+void signal_handler(int sig);
+
 /* strtok functions */
 int token_length(char *string, char *delimiter);
 int number_of_tokens(char *string, char *delimiter);
 char **_strtok(char *line, char *delimiter);
 
 /* args control functions */
-int *get_args(char *line, int last_ret);
+char *get_args(char *line, int last_ret);
 int exec_args(char **commands, char **parameters, int *last_ret);
 int exec2_args(char **commands, char **parameters, int *last_ret);
 int control_args(int *last_ret);
@@ -29,5 +32,11 @@ int control_args(int *last_ret);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+
+/* string functions */
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif
