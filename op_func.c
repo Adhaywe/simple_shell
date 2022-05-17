2e37ce7cb9d5431b4a2634594501585e4bc31be7
Adam <adamghebrezgi@gmail.com>
2022-05-17 17:35:23 +0300

    Function that is used to operate other functions

--- op_func.c
+++ op_func.c
@@ -72,3 +72,38 @@ list_t *add_node_end(list_t **head, char *dir)
 
 	return (new_node);
 }
+
+/**
+ * free_alias_list - Frees a alias_t linked list.
+ * @head: THe head of the alias_t list.
+ */
+void free_alias_list(alias_t *head)
+{
+	alias_t *next;
+
+	while (head)
+	{
+		next = head->next;
+		free(head->name);
+		free(head->value);
+		free(head);
+		head = next;
+	}
+}
+
+/**
+ * free_list - Frees a list_t linked list.
+ * @head: The head of the list_t list.
+ */
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	while (head)
+	{
+		next = head->next;
+		free(head->dir);
+		free(head);
+		head = next;
+	}
+}
