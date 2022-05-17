3a73f9edfc9dee41afa2fe79f3c646119f770dc3
Adam <adamghebrezgi@gmail.com>
2022-05-17 17:17:09 +0300

    Locates the path of the command

---
+++ find_path.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+char *fill_path_dir(char *path);
+list_t *get_path_dir(char *path);
+
+/**
+ * get_location - Locates a command in the PATH.
+ * @command: The command to locate.
+ *
+ * Return: If an error occurs or the command cannot be located - NULL.
+ *         Otherwise - the full pathname of the command.
+ */
