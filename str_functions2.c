a0edd43ba1f0cdd3ac890d0a5ddfe808579ee4f6
Adam <adamghebrezgi@gmail.com>
2022-05-17 17:01:20 +0300

    String functions

--- str_functions2.c
+++ str_functions2.c
@@ -1,45 +1,27 @@
 #include "shell.h"
 
-/**
- * _strcmp - function that compares two strings
- * @s1: string number one
- * @s2: string number two
- *
- * Return: the difference between the two strings
- *         positive if s1 > s2, 0 if s1 = s2, and negative if s1 < s2
- */
-int _strcmp(char *s1, char *s2)
-{
-	int i = 0;
-
-	while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
-		i++;
-	return (*(s1 + i) - *(s2 + i));
-}
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
 
 /**
- * _strncmp - function thta Compares two strings up to n bytes
- * @s1: Pointer to a string
- * @s2: Pointer to a string
- * @n: The first n bytes of the strings to compare
+ * _strchr - Locates a character in a string.
+ * @s: The string to be searched.
+ * @c: The character to be located.
  *
- * Return: Less than 0 if s1 is shorter than s2
- *         0 if s1 and s2 match
- *         Greater than 0 if s1 is longer than s2
+ * Return: If c is found - a pointer to the first occurence.
+ *         If c is not found - NULL.
  */
-int _strncmp(const char *s1, const char *s2, size_t n)
+char *_strchr(char *s, char c)
 {
-	size_t i;
+	int index;
 
-	for (i = 0; s1[i] && s2[i] && i < n; i++)
+	for (index = 0; s[index]; index++)
 	{
-		if (s1[i] > s2[i])
-			return (s1[i] - s2[i]);
-		else if (s1[i] < s2[i])
-			return (s1[i] - s2[i]);
+		if (s[index] == c)
+			return (s + index);
 	}
-	if (i == n)
-		return (0);
-	else
-		return (-15);
+
+	return (NULL);
 }
