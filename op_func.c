e3d7d8b5a570218cb5e20327d6c79b52326fb776
Adam <adamghebrezgi@gmail.com>
2022-05-17 17:33:35 +0300

    Function that is used to operate other functions

--- op_func.c
+++ op_func.c
@@ -40,3 +40,13 @@ alias_t *add_alias_end(alias_t **head, char *name, char *value)
 
 	return (new_node);
 }
+
+/**
+ * add_node_end - Adds a node to the end of a list_t linked list.
+ * @head: A pointer to the head of the list_t list.
+ * @dir: The directory path for the new node to contain.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
+ */
+
