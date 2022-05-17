db73c38c96bd3095f07ab3789377bcb46727883b
Adam <adamghebrezgi@gmail.com>
2022-05-17 16:49:30 +0300

    Header file

--- shell.h
+++ shell.h
@@ -121,4 +121,15 @@ void free_alias_list(alias_t *head);
 list_t *add_node_end(list_t **head, char *dir);
 void free_list(list_t *head);
 
+void help_all(void);
+void help_alias(void);
+void help_cd(void);
+void help_exit(void);
+void help_help(void);
+void help_env(void);
+void help_setenv(void);
+void help_unsetenv(void);
+void help_history(void);
+
+int proc_file_commands(char *file_path, int *exe_ret);
 #endif /* _SHELL_H_ */
