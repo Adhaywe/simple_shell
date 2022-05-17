da4232c2458267101407e019ec18c0e3bf6af4aa
Adam <adamghebrezgi@gmail.com>
2022-05-17 17:32:51 +0300

    Function that is used to operate other functions

--- op_func.c
+++ op_func.c
@@ -9,3 +9,34 @@
  * Return: If an error occurs - NULL.
  *         Otherwise - a pointer to the new node.
  */
+
+alias_t *add_alias_end(alias_t **head, char *name, char *value)
+{
+	alias_t *new_node = malloc(sizeof(alias_t));
+	alias_t *last;
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->next = NULL;
+	new_node->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (!new_node->name)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->value = value;
+	_strcpy(new_node->name, name);
+
+	if (*head)
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new_node;
+	}
+	else
+		*head = new_node;
+
+	return (new_node);
+}
