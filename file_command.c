ac744e01651d07761d2b90b13fcc1843b399000c
Adam <adamghebrezgi@gmail.com>
2022-05-17 17:09:35 +0300

    Executes file commands

---
+++ file_command.c
@@ -0,0 +1,39 @@
+#include "shell"
+
+/**
+ * cant_open - If the file doesn't exist or lacks proper permissions, print
+ * a cant open error.
+ * @file_path: Path to the supposed file.
+ *
+ * Return: 127.
+ */
+
+int cant_open(char *file_path)
+{
+	char *error, *hist_str;
+	int len;
+
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (127);
+
+	len = _strlen(name) + _strlen(hist_str) + _strlen(file_path) + 16;
+	error = malloc(sizeof(char) * (len + 1));
+	if (!error)
+	{
+		free(hist_str);
+		return (127);
+	}
+
+	_strcpy(error, name);
+	_strcat(error, ": ");
+	_strcat(error, hist_str);
+	_strcat(error, ": Can't open ");
+	_strcat(error, file_path);
+	_strcat(error, "\n");
+
+	free(hist_str);
+	write(STDERR_FILENO, error, len);
+	free(error);
+	return (127);
+}
