3da56625e7f262bf2dc7d989b9cde41a0d3024eb
Adam <adamghebrezgi@gmail.com>
2022-05-17 17:31:35 +0300

    Function that is used to operate other functions

--- op_func.c
+++ op_func.c
@@ -1,10 +1,5 @@
 #include "shell.h"
 
-alias_t *add_alias_end(alias_t **head, char *name, char *value);
-void free_alias_list(alias_t *head);
-list_t *add_node_end(list_t **head, char *dir);
-void free_list(list_t *head);
-
 /**
  * add_alias_end - Adds a node to the end of a alias_t linked list.
  * @head: A pointer to the head of the list_t list.
