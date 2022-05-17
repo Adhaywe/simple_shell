9653329cfb01358966ba1b9bd6128692e612e672
Adam <adamghebrezgi@gmail.com>
2022-05-17 17:18:41 +0300

    Locates the path of the command

--- find_path.c
+++ find_path.c
@@ -7,3 +7,9 @@
  * Return: If an error occurs or the command cannot be located - NULL.
  *         Otherwise - the full pathname of the command.
  */
+char *get_location(char *command)
+{
+	char **path, *temp;
+	list_t *dirs, *head;
+	struct stat st;
+
