1b68fa9221e7e632b165cc551cc374f43f1eb172
Adam <adamghebrezgi@gmail.com>
2022-05-17 17:19:36 +0300

    Locates the path of the command

--- find_path.c
+++ find_path.c
@@ -13,3 +13,35 @@ char *get_location(char *command)
 	list_t *dirs, *head;
 	struct stat st;
 
+	path = _getenv("PATH");
+	if (!path || !(*path))
+		return (NULL);
+
+	dirs = get_path_dir(*path + 5);
+	head = dirs;
+
+	while (dirs)
+	{
+		temp = malloc(_strlen(dirs->dir) + _strlen(command) + 2);
+		if (!temp)
+			return (NULL);
+
+		_strcpy(temp, dirs->dir);
+		_strcat(temp, "/");
+		_strcat(temp, command);
+
+		if (stat(temp, &st) == 0)
+		{
+			free_list(head);
+			return (temp);
+		}
+
+		dirs = dirs->next;
+		free(temp);
+	}
+
+	free_list(head);
+
+	return (NULL);
+}
+
