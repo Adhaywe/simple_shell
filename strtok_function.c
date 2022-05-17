d5fef7711b02dfdec9b1e103520117864831e4bc
Adam <adamghebrezgi@gmail.com>
2022-05-17 16:54:28 +0300

    Function that splits a string into tokens

--- strtok_function.c
+++ strtok_function.c
@@ -21,3 +21,30 @@ int token_len(char *str, char *delim)
 
 	return (len);
 }
+
+/**
+ * count_tokens - Counts the number of delimited
+ *                words contained within a string.
+ * @str: The string to be searched.
+ * @delim: The delimiter character.
+ *
+ * Return: The number of words contained within str.
+ */
+int count_tokens(char *str, char *delim)
+{
+	int index, tokens = 0, len = 0;
+
+	for (index = 0; *(str + index); index++)
+		len++;
+
+	for (index = 0; index < len; index++)
+	{
+		if (*(str + index) != *delim)
+		{
+			tokens++;
+			index += token_len(str + index, delim);
+		}
+	}
+
+	return (tokens);
+}
