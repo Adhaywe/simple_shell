a3284db58c32ffa31ca640163db02160e2c6c6f8
Adam <adamghebrezgi@gmail.com>
2022-05-17 16:57:33 +0300

    String functions

--- str_functions.c
+++ str_functions.c
@@ -1,84 +1,23 @@
 #include "shell.h"
 
-/**
- * _strlen - a function that returns the length of a string
- * @s: character input
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	int cnt;
-
-	cnt = 0;
-	while (*(s + cnt) != '\0')
-		cnt++;
-	return (cnt);
-}
+int _strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
 
 /**
- * *_strcat - function that concatenates two strings
- * @dest: char
- * @src: char
- * Return: a pointer to the resulting string dest
- */
-char *_strcat(char *dest, char *src)
-{
-	int i, j;
-
-	i = j = 0;
-	while (*(dest + i))
-		i++;
-	while ((*(dest + i) = *(src + j)))
-	{
-		i++;
-		j++;
-	}
-	return (dest);
-}
-
-/**
- * _strcpy - copies string from src to dest
+ * _strlen - Returns the length of a string.
+ * @s: A pointer to the characters string.
  *
- * @dest: pointer to destination of string
- * @src: pointer to source string to copy from
- *
- * Return: pointer to dest
+ * Return: The length of the character string.
  */
-char *_strcpy(char *dest, char *src)
+int _strlen(const char *s)
 {
-	int i;
+	int length = 0;
 
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-/**
- * _strncat - function that concatenates two strings
- * @dest: char
- * @src: char
- * @n: integer input
- * Return: pointer dest
- */
-char *_strncat(char *dest, char *src, int n)
-{
-	int i, j;
-
-	i = j = 0;
-	while (*(dest + i))
-		i++;
-	while (j < n && *(src + j))
-	{
-		*(dest + i) = *(src + j);
-		i++;
-		j++;
-	}
-	if (j < n)
-		*(dest + i) = *(src + j);
-	return (dest);
+	if (!s)
+		return (length);
+	for (length = 0; s[length]; length++)
+		;
+	return (length);
 }
