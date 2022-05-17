38a64ecf7a9430f680156fa722325a39f8f253d8
Adam <adamghebrezgi@gmail.com>
2022-05-17 16:53:18 +0300

    Function that splits a string into tokens

---
+++ strtok_function.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * token_len - Locates the delimiter index marking the end
+ *             of the first token contained within a string.
+ * @str: The string to be searched.
+ * @delim: The delimiter character.
+ *
+ * Return: The delimiter index marking the end of
+ *         the intitial token pointed to be str.
+ */
+int token_len(char *str, char *delim)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && *(str + index) != *delim)
+	{
+		len++;
+		index++;
+	}
+
+	return (len);
+}
